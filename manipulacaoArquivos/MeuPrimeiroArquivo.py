d09ce56238fa05960428f4b4f401f4bb29fe60d0
Guilherme Gama <guilhermegamabs@gmail.com>
2025-04-05 16:12:35 -0300

    arquivos read

--- manipulacaoArquivos/MeuPrimeiroArquivo.py
+++ manipulacaoArquivos/MeuPrimeiroArquivo.py
@@ -1,3 +1,5 @@
-with open("primeiro_arquivo.txt", "w") as arquivo:
-    arquivo.write("Hello World!")
+with open("primeiro_arquivo.txt", "r") as arquivo:
+    #conteudo = arquivo.readline() - readLine lê somente a primeira linha
+    for linha in arquivo.readlines():
+        print(linha)
 
