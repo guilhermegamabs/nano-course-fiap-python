b7b6225feeddaadb23e77ac50895e48d249d3e38
Guilherme Gama <guilhermegamabs@gmail.com>
2025-04-05 16:01:37 -0300

    dicionarios

---
+++ dicionarios/funcoesManagment.py
@@ -0,0 +1,40 @@
+def perguntar(): 
+    return input("O que deseja realizar?\n" +
+              "<I> - Para Inserir um usuário\n" +
+              "<P> - Para Pesquisar um usuário\n" +
+              "<E> - Para Excluir um usuário\n" +
+              "<L> - Para Listar usuários: ").upper()
+
+def inserir(dicionario):
+    dicionario[input("Digite o login: ").upper()] = [
+        input("Digite o nome: ").upper(),
+        input("Digite a última data de acesso: "),
+        input("Qual a última estação acessada: ").upper()]
+    
+def pesquisar(dicionario):
+    login = input("Digite o login: ").upper()
+    
+    if(dicionario.get(login)):
+        print("####-####")
+        print("Usuário Encontrado:")
+        print(f"Nome: {dicionario[login][0]}")
+        print(f"Último Acesso: {dicionario[login][1]}")
+        print(f"Estação: {dicionario[login][2]}\n")
+    else:
+        print("Usuário não encontrado.")
+    
+def excluir(dicionario):
+    login = input("Digite o login: ").upper()
+
+    if(dicionario.get(login)):
+        del dicionario[login]
+        print('Usuário excluído com sucesso.')
+    else:
+        print("Usuário não encontrado")        
+
+def listar(dicionario):
+    for usario in dicionario:
+        print("####-####")
+        print(f"Nome: {dicionario[usario][0]}")
+        print(f"Último Acesso: {dicionario[usario][1]}")
+        print(f"Estação: {dicionario[usario][2]}\n")
