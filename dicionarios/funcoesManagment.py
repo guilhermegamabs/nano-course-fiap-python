bb15123b83092d73dfa8dac0c7951c1375995f82
Guilherme Gama <guilhermegamabs@gmail.com>
2025-04-06 12:13:13 -0300

    salvando dados no arquivo

--- dicionarios/funcoesManagment.py
+++ dicionarios/funcoesManagment.py
@@ -10,6 +10,12 @@ def inserir(dicionario):
         input("Digite o nome: ").upper(),
         input("Digite a última data de acesso: "),
         input("Qual a última estação acessada: ").upper()]
+    salvar(dicionario)
+    
+def salvar(dicionario):
+    with open("bd.txt", "a") as arquivo:
+        for chave, valor in dicionario.items():
+            arquivo.write(chave + ": " + str(valor))
     
 def pesquisar(dicionario):
     login = input("Digite o login: ").upper()
