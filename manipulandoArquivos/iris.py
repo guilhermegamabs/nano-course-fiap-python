a612388da46a7f47eb543714e988888ffccaa916
Guilherme Gama <guilhermegamabs@gmail.com>
2025-04-06 13:05:27 -0300

    usando um dataset

---
+++ manipulandoArquivos/iris.py
@@ -0,0 +1,9 @@
+bd = []
+
+with open("manipulandoArquivos/iris.data", "r") as arquivo:
+    for registro in arquivo.readlines():
+        bd.append(registro.split(",")) 
+        
+#print(bd)
+
+print(float(bd[0][0]) + float(bd[0][1]))
